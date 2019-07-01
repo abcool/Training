fb824cbd3296d2c195ab0a45da362465b8801d3c
Abcool <arvindbakshicool@gmail.com>
2019-07-01 22:09:44 +0530

    Added a new Chapter-19 to Let US C with b-a.c

---
+++ C_Training/LetUsC/Chapter-19/b-a.c
@@ -0,0 +1,35 @@
+/*
+Program to read a file and display it's contents along with line numbers before each line
+-----------------------------------------------------------
+Version   Date       Author          Changelog
+-----------------------------------------------------------
+1.0    01/07/2019  Arvind Bakshi    Initial Version
+-----------------------------------------------------------
+Copyright @AbCool Codings....
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  FILE *fp;
+  char ch;
+  int l_no=0;
+  fp=fopen("demo.txt","r");
+  if(fp==NULL){
+    puts("Empty File");
+    exit(1);
+  }
+  while(1){
+    l_no++;
+    if((ch=fgetc(fp))==EOF){
+      break;
+    }else{
+      printf("%d \t",l_no);
+      while((ch=fgetc(fp))!='\n'){
+        printf("%c",ch);
+      }
+      printf("\n");
+    }
+  }
+  fclose(fp);
+  return 0;
+}
