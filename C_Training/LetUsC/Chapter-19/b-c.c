e8a51e14b4e5fab7310bd3dc8808323efe5b6706
Abcool <arvindbakshicool@gmail.com>
2019-07-02 21:12:22 +0530

    Modified structure logic in b-c.c using **

--- C_Training/LetUsC/Chapter-19/b-c.c
+++ C_Training/LetUsC/Chapter-19/b-c.c
@@ -12,15 +12,15 @@ Copyright @AbCool Codings....
 struct student{
   char name[80];
   int age;
-}*s=NULL;
-void add(struct student *, int);
+};
+void add(struct student **, int);
 int main(){
   FILE *fp;
   int r,i=0;
   puts("Enter number of records to write");
   scanf("%d",&r);
-  //struct student *s=NULL;
-  add(s,r);
+  struct student *s=NULL;
+  add(&s,r);
   fp = fopen("EMP.dat","wb");
   while(i<r){
     fwrite(&s[i],sizeof(s[i]),1,fp);
@@ -34,15 +34,15 @@ int main(){
   while(fread())*/
   return 0;
 }
-void add(struct student *t, int n){
-  t= (struct student *)malloc(n* sizeof(struct student));
+void add(struct student **t, int n){
+  *t= (struct student *)malloc(n* sizeof(struct student));
   int i=0;
   while(i<n){
     printf("Enter student %d details\n",i+1);
     puts("Enter student's name");
-    scanf("%[^\n]s",(t+i)->name);
+    scanf("%[^\n]s",(*(t+i))->name);
     puts("Enter student's age");
-    scanf("%d",&(t+i)->age);
+    scanf("%d",&(*(t+i))->age);
     i++;
   }
 }
