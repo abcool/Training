b2d051de68a48212b67c607677669d5b5f623099
Er. Arvind Bakshi <38808197+abcool@users.noreply.github.com>
2020-02-14 17:41:00 +0530

    Day 11: 2D Arrays

---
+++ Hackerrank/30DaysofCode/day11.java
@@ -0,0 +1,98 @@
+/**
+@author: Arvind Bakshi
+
+Given a 6x6 2D Array, A: 
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+a b c
+  d
+e f g
+There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+
+Sample Input
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+19
+
+Explanation
+A contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+
+The hourglass with the maximum sum (19) is:
+
+2 4 4
+  2
+1 2 4
+
+**/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
+        int f=-999;
+        for(int r=0;r<4;r++){
+            for(int c=0;c<4;c++){
+                int s=0;
+                s+=arr[r][c]+arr[r][c+1]+arr[r][c+2]
+                        +arr[r+1][c+1]
+                   +arr[r+2][c]+arr[r+2][c+1]+arr[r+2][c+2];
+                   if(f<s){
+                       f=s;
+                   }
+            }
+        }
+        System.out.println(f);
+    }
+}
