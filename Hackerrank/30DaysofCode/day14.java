df356ca5173f2001fd3f053bf21d3f33c61bad96
Er. Arvind Bakshi <38808197+abcool@users.noreply.github.com>
2020-02-17 15:24:25 +0530

    Day 14: Scope

---
+++ Hackerrank/30DaysofCode/day14.java
@@ -0,0 +1,16 @@
+/**
+author: Arvind
+Task
+Complete the Difference class by writing the following:
+   . A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+   . A computeDifference method that finds the maximum absolute difference between any 2 numbers in N 
+   and stores it in the maximumDifference instance variable. 
+
+Sample Input
+3
+1 2 5
+
+Sample Output
+4
+
+*/
