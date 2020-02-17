e342db95c65a7a79b8afbd2024594700885d98cd
Er. Arvind Bakshi <38808197+abcool@users.noreply.github.com>
2020-02-17 17:34:34 +0530

    Day 16: Exceptions - String to Integer

---
+++ Hackerrank/30DaysofCode/day16.java
@@ -0,0 +1,38 @@
+/**
+@author: Arvind
+Task
+Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String.
+
+Note: You must use the String-to-Integer and exception handling constructs built into your submission language. 
+If you attempt to use loops/conditional statements, you will get a 0 score.
+Sample Input 0
+3
+
+Sample Output 0
+3
+
+Sample Input 1
+za
+
+Sample Output 1
+Bad String
+
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try{
+            System.out.println(Integer.parseInt(S));
+        }catch(Exception e){
+            System.out.println("Bad String");
+        }
+    }
+}
