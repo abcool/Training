bda84dbb9bc5e32ae1b216b5c32cc62bf65bc0d5
Er. Arvind Bakshi <38808197+abcool@users.noreply.github.com>
2020-02-17 15:25:07 +0530

    Day 14: Scope

--- Hackerrank/30DaysofCode/day14.java
+++ Hackerrank/30DaysofCode/day14.java
@@ -14,3 +14,50 @@ Sample Output
 4
 
 */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    Difference(int elements[]){
+        this.elements = elements;
+    }
+	// Add your code here
+    public void computeDifference(){
+        int small,large;
+        small = large = elements[0];
+        for(int i=0;i<elements.length;i++){
+            if(small>elements[i]){
+                small=elements[i];
+            }
+            if(large<elements[i]){
+                large=elements[i];
+            }
+        }
+        this.maximumDifference = large-small;
+    }
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
