fda3f64546d9a7f01c842049441d30b9bec4012c
Arvind Bakshi <arvindbakshicool@gmail.com>
2021-12-28 20:23:48 +0530

    Added Catalog, CatalogItems. Added UI form. Also added setting header and cookie in response.

--- Java EE 8 Tarining/src/main/java/com/abcool/servlet/CatalogServlet.java
+++ Java EE 8 Tarining/src/main/java/com/abcool/servlet/CatalogServlet.java
@@ -1,8 +1,11 @@
 package com.abcool.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,8 +36,35 @@ public class CatalogServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+		Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+		response.setHeader("Catalog Response Header", "Catalog Item is "+name);
+		response.addCookie(new Cookie("CatalogCookie","CatalogCookie"));
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head></head>");
+		out.println("<body><table border='solid'>");
+		out.println("<tr>"
+				+ "<th>Name:</th>"
+				+"<th>Manufacturer</th>"
+				+"<th>SKU</th>"
+				+ "</tr>");
+		for(CatalogItem item : Catalog.getItems()) {
+			out.println("<tr>"
+					+"<td>"
+					+item.getName()
+					+ "</td>"
+					+"<td>"
+					+item.getManufacturer()
+					+ "</td>"
+					+"<td>"
+					+item.getSku()
+					+ "</td>"
+					+ "</tr>");
+		}
+		out.println("</table></body></html>");
 	}
 
 }
