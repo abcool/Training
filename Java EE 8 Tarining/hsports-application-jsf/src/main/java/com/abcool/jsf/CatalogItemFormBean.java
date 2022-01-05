8169fed40515d58b1b3e202141c1b6e89153b6ed
Arvind Bakshi <arvindbakshicool@gmail.com>
2022-01-06 02:04:04 +0530

    Rearranged folders

---
+++ Java EE 8 Tarining/hsports-application-jsf/src/main/java/com/abcool/jsf/CatalogItemFormBean.java
@@ -0,0 +1,40 @@
+package com.abcool.jsf;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named("backingBean")
+@RequestScoped
+public class CatalogItemFormBean {
+	private String name,manufacturer,sku;
+	
+
+	public CatalogItemFormBean() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+}
