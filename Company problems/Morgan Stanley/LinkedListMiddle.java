9dade5a6a879faf2d9d69e3836d6d60532b6a072
Arvind Bakshi <arvindbakshicool@gmail.com>
2022-02-14 01:47:09 +0530

    Added Finding middle element in a linked list

---
+++ Company problems/Morgan Stanley/LinkedListMiddle.java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class Solution
+{
+    int getMiddle(Node head)
+    {
+        Node n1=head,n2=head;
+         while(n1!=null && n1.next!=null){
+             n1=n1.next.next;
+             n2=n2.next;
+         }
+         return n2.data;
+    }
+}
+
+class LinkedListMiddle{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            //printList(head); 
+            t--;
+        }
+    } 
+} 
+
