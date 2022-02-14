6cb22b8a646c9361055816e649cd2ac9a531e52c
Arvind Bakshi <arvindbakshicool@gmail.com>
2022-02-14 12:08:36 +0530

    Added program Inorder Successor in BST to morgan stanley

---
+++ Company problems/Morgan Stanley/InorderSuccessor.java
@@ -0,0 +1,168 @@
+/*
+Given a BST, and a reference to a Node x in the BST. Find the Inorder Successor of the given node in the BST.
+
+Example 1:
+
+Input:
+      2
+    /   \
+   1     3
+K(data of x) = 2
+Output: 3 
+Explanation: 
+Inorder traversal : 1 2 3 
+Hence, inorder successor of 2 is 3.
+
+Example 2:
+
+Input:
+             20
+            /   \
+           8     22
+          / \
+         4   12
+            /  \
+           10   14
+K(data of x) = 8
+Output: 10
+Explanation:
+Inorder traversal: 4 8 10 12 14 20 22
+Hence, successor of 8 is 10.
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+Expected Time Complexity: O(Height of the BST).
+Expected Auxiliary Space: O(1).
+
+
+Constraints:
+1 <= N <= 1000, where N is number of nodes
+*/
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Solution
+{
+    ArrayList<Node> list = new ArrayList<>();
+    public void inOrder(Node root){
+        if(root!=null){
+            inOrder(root.left);
+            list.add(root);
+            inOrder(root.right);
+        }
+    }
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x)
+         {
+            inOrder(root);
+            for(int i=0;i<list.size();i++){
+                if(list.get(i).data==x.data && i<list.size()-1)
+                    return list.get(i+1);
+            }
+            return null;
+        }
+}
+
+public class InorderSuccessor {
+    
+    static Node buildTree(String str)
+    {
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+
+    public static Node search(Node root,int x){
+		if(root==null||root.data==x)
+			return root;
+		if(root.data>x)
+			return search(root.left,x);
+		return search(root.right,x);
+	}
+
+	public static void main (String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            int t=Integer.parseInt(br.readLine());
+            
+            while(t > 0){
+                String s = br.readLine();
+                Node root = buildTree(s);
+                
+                int k=Integer.parseInt(br.readLine());
+                Node kNode = search(root,k);
+                
+                Solution g = new Solution();
+                Node x = g.inorderSuccessor(root,kNode);
+                
+                if(x==null) System.out.println("-1");
+                else System.out.println(x.data);
+                t--;
+            }
+    }
+}
