59fc1f478296f22ab38f1328b2ab048479f1959b
Arvind Bakshi <arvindbakshicool@gmail.com>
2024-05-22 17:37:56 +0530

    Fixed test case failing for endpoint: http://localhost:7000/product-composite/abcd

--- packt/ch03/microservices/product-composite-service/src/main/java/edu/learning/microservices/composite/product/services/ProductCompositeServiceImpl.java
+++ packt/ch03/microservices/product-composite-service/src/main/java/edu/learning/microservices/composite/product/services/ProductCompositeServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 @RestController
 public class ProductCompositeServiceImpl implements IProductComposite {
     private final ServiceUtility serviceUtility;
-    private final ProductCompositeHelper helper;
+    private ProductCompositeHelper helper;
 
     public ProductCompositeServiceImpl(ServiceUtility serviceUtility, ProductCompositeHelper helper) {
         this.serviceUtility = serviceUtility;
