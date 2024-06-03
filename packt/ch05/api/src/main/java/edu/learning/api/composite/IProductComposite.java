3ecd1607d586513070fbf0b83e5f1f3921548bbb
Arvind Bakshi <arvindbakshicool@gmail.com>
2024-06-04 00:15:01 +0530

    Fixed swagger-ui page giving 404

--- packt/ch05/api/src/main/java/edu/learning/api/composite/IProductComposite.java
+++ packt/ch05/api/src/main/java/edu/learning/api/composite/IProductComposite.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Tag(name = "Product composite", description = "REST API for composite product information")
+@Tag(name = "ProductComposite", description = "REST API for composite product information")
 public interface IProductComposite {
 
 
--- packt/ch05/microservices/product-composite-service/src/main/java/edu/learning/microservices/composite/product/ProductCompositeServiceApplication.java
+++ packt/ch05/microservices/product-composite-service/src/main/java/edu/learning/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,7 +1,5 @@
 package edu.learning.microservices.composite.product;
 
-import edu.learning.microservices.composite.product.config.APIDocsConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +14,6 @@ public class ProductCompositeServiceApplication {
 	RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
-
-	@Autowired
-	APIDocsConfig docsConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
