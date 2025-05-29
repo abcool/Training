27cc5e8e33259200380298e5b327a85f65afe5e0
Arvind Bakshi <arvindbakshicool@gmail.com>
2025-05-29 18:33:39 +0530

    Initial project setup for spring security.

---
+++ Spring_Security/EazyBankApplication/src/test/java/edu/learning/EazyBankApplication/EazyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.learning.EazyBankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
