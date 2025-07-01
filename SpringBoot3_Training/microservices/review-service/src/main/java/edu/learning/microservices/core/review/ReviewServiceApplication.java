f2d153200646594da4576fdbd1959f54216a2735
Arvind Bakshi <arvindbakshicool@gmail.com>
2025-07-01 21:27:28 +0530

    Initial project setup. Created microservices module with 4 components 1. product-service 2. review-service 3. recommendation-service 4. product-composite-service

---
+++ SpringBoot3_Training/microservices/review-service/src/main/java/edu/learning/microservices/core/review/ReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.learning.microservices.core.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
